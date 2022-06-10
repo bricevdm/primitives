41257fb243a7483b10bf62f7807111b6b34aca6f
Brice Vandemoortele <brice@starberry.games>
2022-06-10 16:27:05 +0200

    barycentric interpolation, incenter, circumcenter, closest point, projected position

--- Primitives/Triangle.cs
+++ Primitives/Triangle.cs
@@ -3,80 +3,118 @@ using UnityEngine;
 
 namespace WizardsRepublic.Primitives
 {
-    public class Triangle : IEquatable<Triangle>
+  public class Triangle : IEquatable<Triangle>
+  {
+    public Vertex A { get; }
+    public Vertex B { get; }
+    public Vertex C { get; }
+
+    public bool IsBad { get; set; }
+
+    public Triangle(Vertex a, Vertex b, Vertex c)
     {
-        public Vertex A { get; }
-        public Vertex B { get; }
-        public Vertex C { get; }
-        
-        public bool IsBad { get; set; }
-
-        public Triangle(Vertex a, Vertex b, Vertex c)
-        {
-            A = a;
-            B = b;
-            C = c;
-        }
-
-        public bool ContainsVertex(Vector3 v)
-        {
-            return Vector3.Distance(v, A.Position) < 0.01f
-                   || Vector3.Distance(v, B.Position) < 0.01f
-                   || Vector3.Distance(v, C.Position) < 0.01f;
-        }
-
-        public bool CircumCircleContains(Vector3 v)
-        {
-            Vector3 a = A.Position;
-            Vector3 b = B.Position;
-            Vector3 c = C.Position;
-
-            float ab = a.sqrMagnitude;
-            float cd = b.sqrMagnitude;
-            float ef = c.sqrMagnitude;
-
-            float circumX = (ab * (c.y - b.y) + cd * (a.y - c.y) + ef * (b.y - a.y)) /
-                            (a.x * (c.y - b.y) + b.x * (a.y - c.y) + c.x * (b.y - a.y));
-            float circumY = (ab * (c.x - b.x) + cd * (a.x - c.x) + ef * (b.x - a.x)) /
-                            (a.y * (c.x - b.x) + b.y * (a.x - c.x) + c.y * (b.x - a.x));
-
-            Vector3 circum = new Vector3(circumX / 2, circumY / 2);
-            float circumRadius = Vector3.SqrMagnitude(a - circum);
-            float dist = Vector3.SqrMagnitude(v - circum);
-            return dist <= circumRadius;
-        }
-
-        public static bool operator ==(Triangle left, Triangle right)
-        {
-            if (left == null || right == null) return false;
-            return (left.A == right.A || left.A == right.B || left.A == right.C)
-                   && (left.B == right.A || left.B == right.B || left.B == right.C)
-                   && (left.C == right.A || left.C == right.B || left.C == right.C);
-        }
-
-        public static bool operator !=(Triangle left, Triangle right)
-        {
-            return !(left == right);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is Triangle t)
-            {
-                return this == t;
-            }
-
-            return false;
-        }
-
-        public bool Equals(Triangle t)
-        {
-            return this == t;
-        }
-
-        public override int GetHashCode()
-        {
-            return A.GetHashCode() ^ B.GetHashCode() ^ C.GetHashCode();
-        }
+      A = a;
+      B = b;
+      C = c;
     }
+
+    public bool ContainsVertex(Vector3 v)
+    {
+      return Vector3.Distance(v, A.Position) < 0.01f
+             || Vector3.Distance(v, B.Position) < 0.01f
+             || Vector3.Distance(v, C.Position) < 0.01f;
+    }
+
+    public bool CircumCircleContains(Vector3 v)
+    {
+      Vector3 a = A.Position;
+      Vector3 b = B.Position;
+      Vector3 c = C.Position;
+
+      float ab = a.sqrMagnitude;
+      float cd = b.sqrMagnitude;
+      float ef = c.sqrMagnitude;
+
+      float circumX = (ab * (c.y - b.y) + cd * (a.y - c.y) + ef * (b.y - a.y)) /
+                      (a.x * (c.y - b.y) + b.x * (a.y - c.y) + c.x * (b.y - a.y));
+      float circumY = (ab * (c.x - b.x) + cd * (a.x - c.x) + ef * (b.x - a.x)) /
+                      (a.y * (c.x - b.x) + b.y * (a.x - c.x) + c.y * (b.x - a.x));
+
+      Vector3 circum = new Vector3(circumX / 2, circumY / 2);
+      float circumRadius = Vector3.SqrMagnitude(a - circum);
+      float dist = Vector3.SqrMagnitude(v - circum);
+      return dist <= circumRadius;
+    }
+
+    public static bool operator ==(Triangle left, Triangle right)
+    {
+      if (left == null || right == null) return false;
+      return (left.A == right.A || left.A == right.B || left.A == right.C)
+             && (left.B == right.A || left.B == right.B || left.B == right.C)
+             && (left.C == right.A || left.C == right.B || left.C == right.C);
+    }
+
+    public static bool operator !=(Triangle left, Triangle right)
+    {
+      return !(left == right);
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (obj is Triangle t)
+      {
+        return this == t;
+      }
+
+      return false;
+    }
+
+    public bool Equals(Triangle t)
+    {
+      return this == t;
+    }
+
+    public override int GetHashCode()
+    {
+      return A.GetHashCode() ^ B.GetHashCode() ^ C.GetHashCode();
+    }
+
+    public Vector3 GetBarycentricCoordinates(Vector3 point) 
+      => point.GetBarycentricCoordinates(A.Position, B.Position, C.Position);
+
+    public Vector3 GetTriangleIncenter() 
+      => GetTriangleIncenter(A.Position, B.Position, C.Position);
+    
+    public static Vector3 GetTriangleIncenter(Vector3 vertexA, Vector3 vertexB, Vector3 vertexC)
+    {
+      float edgeA = Vector3.Distance(vertexB, vertexC);
+      float edgeB = Vector3.Distance(vertexA, vertexC);
+      float edgeC = Vector3.Distance(vertexA, vertexB);
+      float perimeter = edgeA + edgeB + edgeC;
+
+      return new Vector3(
+        edgeA * vertexA.x + edgeB * vertexB.x + edgeC * vertexC.x,
+        edgeA * vertexA.y + edgeB * vertexB.y + edgeC * vertexC.y,
+        edgeA * vertexA.z + edgeB * vertexB.z + edgeC * vertexC.z
+      ) / perimeter;
+    }
+    
+    public Vector3 GetTriangleCircumcenter(out float circumsphereRadius)
+      => GetTriangleCircumcenter(A.Position, B.Position, C.Position, out circumsphereRadius);
+    
+    public static Vector3 GetTriangleCircumcenter(Vector3 vertexA, Vector3 vertexB, Vector3 vertexC, out float circumsphereRadius)
+    {
+      Vector3 ac = vertexC - vertexA;
+      Vector3 ab = vertexB - vertexA;
+      Vector3 abXac = Vector3.Cross(ab, ac);
+
+      Vector3 toCircumsphereCenter = (Vector3.Cross(abXac,ab) * ac.sqrMagnitude 
+                                      + Vector3.Cross(ac,abXac) * ab.sqrMagnitude) 
+                                     / (2f * abXac.sqrMagnitude);
+      
+      circumsphereRadius = toCircumsphereCenter.magnitude;
+      
+      return vertexA + toCircumsphereCenter;
+    }
+  }
 }
---
+++ Primitives/VectorExtensionMethods.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace WizardsRepublic.Primitives
+{
+  public static class VectorExtensionMethods
+  {
+    public static Vector3 GetBarycentricCoordinates(this Vector3 point, Vector3 v0, Vector3 v1, Vector3 v2)
+    {
+      Vector3 v10 = v1 - v0;
+      Vector3 p0 = point - v0;
+      Vector3 v21 = v2 - v1;
+      Vector3 p1 = point - v1;
+      Vector3 v02 = v0 - v2;
+      Vector3 p2 = point - v2;
+      Vector3 nor = Vector3.Cross(v10, v02);
+
+      Vector3 q = Vector3.Cross(nor, p0);
+      float d = 1.0f / Vector3.SqrMagnitude(nor);
+      float u = d * Vector3.Dot(q, v02);
+      float v = d * Vector3.Dot(q, v10);
+      float w = 1.0f - u - v;
+
+      if (u < 0.0)
+      {
+        w = Mathf.Clamp01(Vector3.Dot(p2, v02) / Vector3.SqrMagnitude(v02));
+        u = 0.0f;
+        v = 1.0f - w;
+      }
+      else if (v < 0.0)
+      {
+        u = Mathf.Clamp01(Vector3.Dot(p0, v10) / Vector3.SqrMagnitude(v10));
+        v = 0.0f;
+        w = 1.0f - u;
+      }
+      else if (w < 0.0)
+      {
+        v = Mathf.Clamp01(Vector3.Dot(p1, v21) / Vector3.SqrMagnitude(v21));
+        w = 0.0f;
+        u = 1.0f - v;
+      }
+
+      return new Vector3(w, u, v);
+    }
+
+    public static Vector3 GetProjectedPosition(this Vector3 position, Transform projectionTransform)
+    {
+      var forward = projectionTransform.forward;
+
+      Plane plane = new Plane(forward, projectionTransform.position);
+      return position - forward * plane.GetDistanceToPoint(position);
+    }
+    
+    // https://www.shadertoy.com/view/ttfGWl by Inigo Quilez
+    public static Vector3 GetClosestPointToTriangle(this Vector3 p, Vector3 v0, Vector3 v1, Vector3 v2, out Vector3 barycentricCoordinates)
+    {
+      barycentricCoordinates = p.GetBarycentricCoordinates(v0, v1, v2);
+      return barycentricCoordinates.z * v1 + barycentricCoordinates.y * v2 + barycentricCoordinates.x * v0;
+    }
+  }
+}
